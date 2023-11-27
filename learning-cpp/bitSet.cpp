12ff8e86c163458065279c19c45e1b14a5ec6efc
ROBIND KUMAR <kumarrobind386@gmail.com>
2023-11-27 17:13:57 +0530

    Updated bitSet for TestCases

--- learning-cpp/bitSet.cpp
+++ learning-cpp/bitSet.cpp
@@ -1,17 +1,28 @@
 // Program to check if Kth bit is set or not.
 //Set mean binary bit at kth place is 1.
+
+// TestCase
+// 5 1
+// yes
+
+// 8 2
+// no
+
+// 0 3
+// no
+
+
 #include <iostream>
 
 int main()
 {
-    int n=9,k=4;
+    int n,k;
+    std::cin>>n>>k;
 
     int kBin=pow(2,k-1);
-    // std::cout<<(kBin&n)<<std::endl;
-
     int resSet=kBin&n;
 
-    std::cout<<((resSet==kBin)?"Yes":"No");
+    std::cout<<((resSet==kBin)?"yes":"no");
 
     return 0;
 }

457fc29e49fc7e1b431c59da27123148bd3963a1
ROBIND KUMAR <kumarrobind386@gmail.com>
2023-12-18 20:00:03 +0530

    Added program to find largest element in an array

---
+++ learning-cpp/largestElmArray.cpp
@@ -0,0 +1,27 @@
+// Program to find largest element in an array.
+#include<iostream>
+
+int largestElm(int arr[],int n){
+    int max=arr[0];
+    int maxIndex=0;
+    for(int i=0;i<n;i++)
+    {
+        if(max<arr[i])
+        {
+            max=arr[i];
+            maxIndex=i;
+        }
+    }
+
+    return maxIndex;
+}
+int main()
+{
+    int arr[]={10,20,5,3,6,26,7};
+    std::cout<<largestElm(arr,7);
+
+    // std::cout<<arr[6];
+
+
+    return 0;
+}

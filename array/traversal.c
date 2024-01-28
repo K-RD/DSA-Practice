b85ec392f407b4a6ba617a2121eeec52bbc1559b
ROBIND KUMAR <kumarrobind386@gmail.com>
2024-01-28 17:36:52 +0530

    Array Traversal code in c

---
+++ array/traversal.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void traversal(int *, int);
+
+int main()
+{
+    int arr[5] = {2, 6, 8, 5, 3};
+    int size = 5;
+    printf("Printing the array:\n");
+    traversal(arr, size);
+    return 0;
+}
+
+void traversal(int *array, int size)
+{
+    printf("the address of array variabl is %p \n", array);
+    printf("the value of array variabl is %d \n", *array);
+    for (int i = 0; i < size; i++)
+    {
+        printf("arr[%d] = %d\n", i, array[i]);
+    }
+}

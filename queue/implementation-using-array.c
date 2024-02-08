b632bdbf52f69de440c26d8e7db9b635bb11a7ae
ROBIND KUMAR <kumarrobind386@gmail.com>
2024-02-08 12:18:25 +0530

    Added the Dequeue function

--- queue/implementation-using-array.c
+++ queue/implementation-using-array.c
@@ -12,6 +12,12 @@ struct queue
     int *arr;
 };
 
+// Function to check whether queue is empty or not.
+int isEmpty(struct queue *q)
+{
+    return (q->front == -1 && q->rear == -1);
+}
+
 // Function to check whether a queue is full or not.
 int isFull(struct queue *q)
 {
@@ -27,7 +33,7 @@ void creation(struct queue *q, unsigned int size)
     q->arr = (int *)malloc(size * sizeof(int));
 }
 
-// function to insert an element in queue---> Enqueue.
+// Function to insert an element in queue---> Enqueue.
 void enqueue(struct queue *q, int data)
 {
     if (isFull(q))
@@ -49,15 +55,38 @@ void enqueue(struct queue *q, int data)
     }
 }
 
+// Function to delete an element from the queue--> Dequeue.
+int dequeue(struct queue *q)
+{
+    if (isEmpty(q))
+    {
+        printf("Queue underflow! Cannot dequeue from an empty queue.");
+        return -1;
+    }
+    else
+    {
+        int temp = q->arr[q->front];
+        printf("Element %d deleted successfully from the queue.\n", temp);
+        q->front++;
+        return temp;
+    }
+}
 // Traversal in queue.
 void traversal(struct queue *q)
 {
-    int temp = q->front;
-    do
+    if (isEmpty(q))
     {
-        printf("Elements: %d \n", q->arr[temp]);
-        temp++;
-    } while (temp > !q->size - 1 && temp != q->rear);
+        printf("The Queue is empty.\n");
+    }
+    else
+    {
+        int temp = q->front;
+        do
+        {
+            printf("Elements: %d \n", q->arr[temp]);
+            temp++;
+        } while (temp <= q->size - 1 && temp <= q->rear);
+    }
 }
 
 int main()
@@ -67,10 +96,13 @@ int main()
     creation(q, 10);
     enqueue(q, 20);
     enqueue(q, 30);
+    int temp = dequeue(q);
     enqueue(q, 40);
     enqueue(q, 50);
+    int temp2 = dequeue(q);
     enqueue(q, 60);
     enqueue(q, 70);
+    int temp3 = dequeue(q);
     enqueue(q, 80);
     enqueue(q, 90);
     enqueue(q, 100);

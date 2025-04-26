8eb32c35855a0f9f30a2d96ff2481814965497a1
ROBIND KUMAR <kumarrobind386@gmail.com>
2025-04-26 22:31:50 +0530

    optimized version of 128. Longest Consecutive Sequence

--- leetcode/128-Longest-Consecutive-Sequence.cpp
+++ leetcode/128-Longest-Consecutive-Sequence.cpp
@@ -1,23 +1,27 @@
-#include<iostream>
-#include<vector>
-#include<set>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <unordered_set>
 using namespace std;
 
 class Solution
 {
 public:
-  int longestConsecutive(vector<int> &nums)
+  int longestConsecutive2(vector<int> &nums)
   {
-    if (nums.empty())return 0;
-    set<int> freq(nums.begin(), nums.end());
+    if (nums.empty())
+      return 0;
+    set<int> freq(nums.begin(), nums.end()); // time : O(n log n )
 
     int longestSequence = 0;
     int currSeqLength = 0;
 
     int currValue = *freq.begin();
 
-    for(int a: freq){
-      if(a==currValue){
+    for (int a : freq)
+    { // time : O(n)
+      if (a == currValue)
+      {
         currSeqLength++;
         currValue++;
       }
@@ -29,33 +33,59 @@ public:
       }
     }
     longestSequence = max(longestSequence, currSeqLength);
-    
+
+    return longestSequence;
+  }
+
+  int longestConsecutive(vector<int> &nums)
+  {
+    unordered_set<int> freq(nums.begin(), nums.end());
+
+    int longestSequence = 0;
+    int currSeqLength = 0;
+
+    for (int num : freq)
+    {
+      if (!freq.count(num-1))
+      {
+        int currValue = num;
+        currSeqLength = 1;
+
+        while(freq.count(currValue + 1)){
+          currValue++;
+          currSeqLength++;
+        }
+      }
+      longestSequence = max(longestSequence, currSeqLength);
+    }
     return longestSequence;
   }
 };
 
-void printVec(const vector<int> &nums){
+void printVec(const vector<int> &nums)
+{
   cout << "Vector: ";
-  for(int num: nums){
+  for (int num : nums)
+  {
     cout << num << " ";
   }
 }
 
-int main(){
+int main()
+{
   vector<int> nums1{100, 4, 200, 1, 3, 2};
   Solution s;
 
   printVec(nums1);
   cout << ": " << s.longestConsecutive(nums1) << endl;
-  
+
   vector<int> nums2{0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
   printVec(nums2);
   cout << ": " << s.longestConsecutive(nums2) << endl;
-  
+
   vector<int> nums3{1, 0, 1, 2};
   printVec(nums3);
   cout << ": " << s.longestConsecutive(nums3) << endl;
-
 
   return 0;
 }
